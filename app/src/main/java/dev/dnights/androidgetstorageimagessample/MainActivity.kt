849b21205f851b364e9069b48d83e3e67cb5ae0a
hyun dong han <guehgk456@gmail.com>
2020-11-16 11:51:56 +0900

    add get Image by Intent

--- app/src/main/java/dev/dnights/androidgetstorageimagessample/MainActivity.kt
+++ app/src/main/java/dev/dnights/androidgetstorageimagessample/MainActivity.kt
@@ -1,11 +1,49 @@
 package dev.dnights.androidgetstorageimagessample
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.dnights.androidgetstorageimagessample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_PICK
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            startActivityForResult(intent, REQUEST_CODE_GET_IMAGES)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_GET_IMAGES) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    val ins = contentResolver.openInputStream(data?.data!!)
+                    val img = BitmapFactory.decodeStream(ins)
+                    ins?.close()
+                    // 이미지 표시
+                    binding.image.setImageBitmap(img)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_GET_IMAGES = 1339
     }
 }
